f85704db5fd4909a290ba12419282700b9f89721
Lyu Han <lvhan_028@163.com>
2024-10-15 15:43:48 +0800

    pybind gemm (#3)

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cmake -G Ninja .. \
+cmake .. \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
-    -DCMAKE_CUDA_COMPILER=/usr/local/cuda-11.8/bin/nvcc \
     -DCMAKE_CUDA_FLAGS="-lineinfo" \
     -DUSE_NVTX=ON \
+    -DBUILD_TEST=ON \
     -DFETCHCONTENT_UPDATES_DISCONNECTED=ON \
     -DLMDEPLOY_ASAN_ENABLE=OFF \
     -DLMDEPLOY_UBSAN_ENABLE=OFF \
---
+++ src/turbomind/api/python/linear.h
@@ -0,0 +1,38 @@
+// Copyright (c) OpenMMLab. All rights reserved.
+
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <memory>
+#include <cuda_runtime.h>
+#include "src/turbomind/kernels/gemm/types.h"
+#include "src/turbomind/utils/tensor.h"
+
+namespace turbomind {
+
+
+enum class WeightType : int
+{
+    kFP32,
+    kFP16,
+    kFP8,  // not supported yet
+    kBF16,
+    kINT8,
+    kINT4
+};
+
+
+class Linear {
+public:
+    Linear(size_t input_dims, size_t output_dims, int w_bit, int group_size);
+    void post_init(std::shared_ptr<Tensor> qweight, const Tensor& scales, const Tensor& qzeros,
+                   bool simt);
+    void forward(const Tensor& in, Tensor& out);
+    ~Linear() {}
+
+private:
+    struct Impl;
+    std::shared_ptr<Impl> impl_;
+};
+};
